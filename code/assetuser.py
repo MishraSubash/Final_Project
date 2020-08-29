43128ca1d2300ea2c8be90b15f620e56275691bb
KevinLacap <61166829+KevinLacap@users.noreply.github.com>
2020-08-29 10:21:01 -0400

    Add files via upload

--- code/assetuser.py
+++ code/assetuser.py
@@ -7,66 +7,66 @@ from bs4 import BeautifulSoup
 from other_functions import *
 import gvars
 
-class assetuser:
-    def __init__(user):
-        user.lockedassets = set() # assets with no defined strategy
-        user.tradeableassets = set() # assets that could be traded today
-        user.availableassets = set() # assets availabe after filter
-        user.usedassets = set() # used assets being traded
-        user.excludedassets = {'SPCE'} # omit assets 
+class assetself:
+    def __init__(self):
+        self.lockedassets = set() # assets with no defined strategy
+        self.tradeableassets = set() # assets that could be traded today
+        self.availableassets = set() # assets availabe after filter
+        self.usedassets = set() # used assets being traded
+        self.excludedassets = {'SPCE'} # omit assets 
 
         try:
-            user.unfilteredassets = set(pd.read_csv(gvars.unfiltered_assets))
+            self.unfilteredassets = set(pd.read_csv(gvars.unfiltered_assets))
             print("unfiltered assets loaded from csv correclty")
         except Exception as e:
             print("Does Not Load Asset!")
             print(e)
             block_thread()
 
-        user.tradeableassets = user.rawassets
+        self.tradeableassets = self.rawassets
 
-        th = threading.Thread(target=user.unlock_assets) # code flows appart
+        th = threading.Thread(target=self.unlock_assets) # code flows appart
         th.start()
 
-    def find_target_asset(user):
+    def find_target_asset(self):
 
         if true:
-            user.availableassets = user.tradeableassets
-            user.availableassets -= user.usedassets
-            user.availableassets -= user.excludedassets
-            user.availableassets -= user.lockedassets
+            self.availableassets = self.tradeableassets
+            self.availableassets -= self.usedassets
+            self.availableassets -= self.excludedassets
+            self.availableassets -= self.lockedassets
 
             try:
-                selectasset = random.choice(list(user.availableAssets)) # select randomly
-                user.usedassets.add(selectasset)
+                selectasset = random.choice(list(self.availableAssets)) # select randomly
+                self.usedassets.add(selectasset)
                 print('select asset: ' + selectedasset)
-                print('%i available assets, %i used assets, %i locked assets\n' % (len(user.availableAssets),len(user.usedAssets),len(user.lockedAssets)))
+                print('%i available assets, %i used assets, %i locked assets\n' % (len(self.availableAssets),len(self.usedAssets),len(self.lockedAssets)))
                 return chosenAsset
             except:
                 print('No selected assets available, waiting for assets to be issued...')
                 time.sleep(60)
 
-    def make_asset_available(user,symbol):
+    def make_asset_available(self,symbol):
 
         try:
-            user.usedassets.remove(symbol)
+            self.usedassets.remove(symbol)
         except exception as ee:
             print('Does not remove %s from used assets, not found' % symbol)
             print(ee)
 
-        user.availableassets.add(symbol)
+        self.availableassets.add(symbol)
         print('asset %s was made available' % symbol)
         time.sleep(1)
 
-    def lock_asset(user,symbol):
+    def lock_asset(self,symbol):
         if type(symbol) is not str:
             raise Exception('symbol is not a string!')
 
         time = datetime.now()
-        user.usedassets.remove(symbol)
-        user.lockedassets.add(symbol)
+        self.usedassets.remove(symbol)
+        self.lockedassets.add(symbol)
 
-    def unlock_assets(user):
+    def unlock_assets(self):
         # function opens the locked assets gradually
 
         print('\nUnlocking service processing')
@@ -75,8 +75,8 @@ class assetuser:
             time_before = datetime.now()-timedelta(minutes=30)
 
 
-            user.tradeableassets = user.tradeableassets.union(user.lockedassets)
-            print('%d locked assets moved to tradeable' % len(user.lockedassets))
-            user.lockedassets = set()
+            self.tradeableassets = self.tradeableassets.union(self.lockedassets)
+            print('%d locked assets moved to tradeable' % len(self.lockedassets))
+            self.lockedassets = set()
 
             time.sleep(gvars.sleeptimes['UA'])
